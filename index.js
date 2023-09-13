c7bf3655e5b75df57f0fa4a032f1a109e071019f
Katie Sayers <kate@student.dev-island.org>
2023-09-13 11:28:19 -1000

    added media queries, worked out tons of bugs, tried getting the hamburger to work

---
+++ index.js
@@ -0,0 +1,20 @@
+const NAV_MENU = document.querySelector(".menu");
+const NAV_MENU_BUTTON = document.querySelector(".hamburger");
+
+function setNavMenuOffsetTop() {
+  const NAV_BAR = document.querySelector(".sidebar__container");
+  NAV_MENU.style.top = NAV_BAR.clientHeight + "px";
+}
+
+function openNavMenu() {
+  /* 
+        Setting display to grid when the button is clicked is a hacky way to prevent
+        the slide out animation from triggering on page load.
+    */
+  NAV_MENU.style.display = "grid";
+  NAV_MENU_BUTTON.classList.toggle("hamburger-open");
+  NAV_MENU.classList.toggle("menu-open");
+}
+
+setNavMenuOffsetTop();
+NAV_MENU_BUTTON.addEventListener("click", openNavMenu);
