5c5aaa21e367a3cec9d53b02945df0515ed78bf9
Katie Sayers <kate@student.dev-island.org>
2023-09-14 20:31:12 -1000

    debugged the menu slide

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function setNavMenuOffsetTop() {
 }
 
 function openNavMenu() {
-  
   /* 
         Setting display to grid when the button is clicked is a hacky way to prevent
         the slide out animation from triggering on page load.
     */
   NAV_MENU.style.display = "grid";
+  setNavMenuOffsetTop();
+
   NAV_MENU_BUTTON.classList.toggle("hamburger-open");
   NAV_MENU.classList.toggle("menu-open");
 }
 
-setNavMenuOffsetTop();
 NAV_MENU_BUTTON.addEventListener("click", openNavMenu);
